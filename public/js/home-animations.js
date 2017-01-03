e84cfa1f0aee61cb219033e20198cc37f896c683
Levi Zitting <levizitting5663@gmail.com>
2017-01-02 20:32:14 -0600

    Adding animations to the homepage and updating Jquery

---
+++ public/js/home-animations.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	scrollAnimation("#home-activities-text", "fadeInLeft");
+	scrollAnimation("#home-activities-img", "fadeInRight");
+	scrollAnimation("#home-about-header", "fadeInUp");
+	scrollAnimation("#home-about-hr", "fadeInUp");
+	scrollAnimation("#home-about-flag", "fadeInLeft");
+	scrollAnimation("#home-about-card", "fadeInRight");
+});
+
