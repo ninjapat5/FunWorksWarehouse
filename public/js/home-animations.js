fea3a84ed11fbbe952599c7ca7f5078a94c9309f
Levi Zitting <levizitting5663@gmail.com>
2017-01-02 20:42:48 -0600

    Overhailing animations and adding Roboto font

--- public/js/home-animations.js
+++ public/js/home-animations.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	scrollAnimation("#home-learn-more", "fadeInUp");
 	scrollAnimation("#home-activities-text", "fadeInLeft");
 	scrollAnimation("#home-activities-img", "fadeInRight");
 	scrollAnimation("#home-about-header", "fadeInUp");
