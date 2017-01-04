d5f91c7dd078d40b371e022949b97149b367c5aa
Levi Zitting <levizitting5663@gmail.com>
2017-01-03 23:14:16 -0600

    Adding animation to contact page

--- public/js/animations/contact-animations.js
+++ public/js/animations/contact-animations.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
 	scrollAnimation("#get-in-touch", "fadeIn");
-	scrollAnimation("#contact-info", "fadeInLeft");
-	scrollAnimation("#map", "fadeInRight");
+	scrollAnimation("#contact-info", "fadeInLeft", 500);
+	scrollAnimation("#map", "fadeInRight", 500);
+  scrollAnimation("#get-in-touch-scroll", "fadeInUp", 1500);
+  scrollAnimation("#connect-header", "fadeIn");
+  scrollAnimation("#connect-facebook", "fadeInUp");
+  scrollAnimation("#connect-twitter", "fadeInUp", 100);
+  scrollAnimation("#connect-instagram", "fadeInUp", 200);
+  scrollAnimation("#connect-google-plus", "fadeInUp", 300);
+  scrollAnimation("#connect-pinterest", "fadeInUp", 400);
+  scrollAnimation("#connect-scroll", "fadeInUp", 1400);
+  scrollAnimation("#form-well", "flipInX", 500);
 });
 
 //material contact form animation
