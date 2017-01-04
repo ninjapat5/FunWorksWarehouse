686605e1f5f3e2ba18c0c3105bc8353e48cbe601
Levi Zitting <levizitting5663@gmail.com>
2017-01-04 16:41:06 -0600

    Changes to reserve and moving css for form to global

--- public/js/global-scripts.js
+++ public/js/global-scripts.js
@@ -37,3 +37,30 @@ function scrollAnimation(element, animation, delay) {
     }
   }, { offset: "80%"});
 }
+
+//material contact form animation
+$('.material-form').find('.form-control').each(function() {
+  var targetItem = $(this).parent();
+  if ($(this).val()) {
+    $(targetItem).find('label').css({
+      'top': '10px',
+      'fontSize': '14px'
+    });
+  }
+})
+$('.material-form').find('.form-control').focus(function() {
+  $(this).parent('.input-block').addClass('focus');
+  $(this).parent().find('label').animate({
+    'top': '10px',
+    'fontSize': '14px'
+  }, 300);
+})
+$('.material-form').find('.form-control').blur(function() {
+  if ($(this).val().length == 0) {
+    $(this).parent('.input-block').removeClass('focus');
+    $(this).parent().find('label').animate({
+      'top': '25px',
+      'fontSize': '18px'
+    }, 300);
+  }
+});
