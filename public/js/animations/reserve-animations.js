14879d60539b873034a299212ba74f8eb841a26b
Levi Zitting <levizitting5663@gmail.com>
2017-01-06 12:10:40 -0600

    Finishing reserve form and reworking animations on all pages

--- public/js/animations/reserve-animations.js
+++ public/js/animations/reserve-animations.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-  scrollAnimation("#reserve-header-text", "fadeIn", 100);
-  scrollAnimation("#small", "fadeInUp", 300);
-  scrollAnimation("#medium", "fadeInUp", 400);
-  scrollAnimation("#large", "fadeInUp", 500);
-  scrollAnimation("#x-large", "fadeInUp", 600);
+  scrollAnimation("#reserve-header-text", "fadeIn", 200, 80);
+  scrollAnimation("#small", "fadeInUp", 300, 80);
+  scrollAnimation("#medium", "fadeInUp", 400, 80);
+  scrollAnimation("#large", "fadeInUp", 500, 80);
+  scrollAnimation("#x-large", "fadeInUp", 600, 80);
 });
