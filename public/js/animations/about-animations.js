a55d0f4bc7a638c12097175cfd859da4a7b3776a
Unknown <ninjapat5@gmail.com>
2017-01-05 19:34:17 -0600

    Adding to about page code

---
+++ public/js/animations/about-animations.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  scrollAnimation("#about-header", "fadeIn", 100);
+  scrollAnimation("#about-p", "fadeInUp", 300);
+  scrollAnimation("#medium", "fadeInUp", 400);
+  scrollAnimation("#large", "fadeInUp", 500);
+  scrollAnimation("#x-large", "fadeInUp", 600);
+});
