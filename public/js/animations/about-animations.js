88a6bbefe694d52724d91741af0eef2f68e964da
Unknown <ninjapat5@gmail.com>
2017-01-06 02:54:44 -0600

    Adding animationes to about page.

--- public/js/animations/about-animations.js
+++ public/js/animations/about-animations.js
@@ -5,8 +5,18 @@ $(document).ready(function() {
   scrollAnimation("#about-p2", "fadeIn", 100);
   scrollAnimation("#panel", "fadeInUp", 400);
   scrollAnimation("#about-scroll", "fadeInUp", 500);
-  scrollAnimation("","")
-  scrollAnimation("","")
-  scrollAnimation("","")
-  scrollAnimation("","")
+  scrollAnimation("#meet-header","fadeIn", 100);
+  scrollAnimation("#team-card0","fadeInLeft");
+  scrollAnimation("#team-card1","fadeInRight", 100);
+  scrollAnimation("#meet-scroll","fadeInUp", 500);
+  scrollAnimation("#faq-header","fadeIn", 100);
+  scrollAnimation("#faq0","fadeInLeft", 200);
+  scrollAnimation("#faq1","fadeInRight", 200);
+  scrollAnimation("#faq2","fadeInLeft", 200);
+  scrollAnimation("#faq3","fadeInUp", 200);
+  scrollAnimation("#faq4","fadeInRight", 200);
+  scrollAnimation("","");
+  scrollAnimation("","");
+  scrollAnimation("","");
+  scrollAnimation("","");
 });
