5906efb7eee03b627ea8344c47fb6bf9154eaadc
Levi Zitting <levizitting5663@gmail.com>
2017-01-06 12:57:52 -0600

    Starting mobile styling

--- public/js/animations/about-animations.js
+++ public/js/animations/about-animations.js
@@ -1,18 +1,16 @@
 $(document).ready(function() {
-  scrollAnimation("#about-header", "fadeIn", 100, 80);
-  scrollAnimation("#about-p", "fadeInUp", 300, 80);
-  scrollAnimation("#about-img", "fadeIn", 0, 80);
-  scrollAnimation("#about-p2", "fadeIn", 100, 80);
-  scrollAnimation("#panel", "fadeInUp", 400, 80);
-  scrollAnimation("#about-scroll", "fadeInUp", 500, 99);
+  scrollAnimation("#about-header", "fadeIn", 100, 99);
+  scrollAnimation("#about-img", "fadeInRight", 800, 99);
+  scrollAnimation("#about-p2", "fadeInLeft", 2000, 99);
+  scrollAnimation("#about-scroll", "fadeInUp",2500, 99);
   scrollAnimation("#meet-header","fadeIn", 100, 80);
   scrollAnimation("#team-card0","fadeInLeft", 0, 80);
-  scrollAnimation("#team-card1","fadeInRight", 100, 80);
+  scrollAnimation("#team-card1","fadeInRight", 0, 80);
   scrollAnimation("#meet-scroll","fadeInUp", 500, 99);
   scrollAnimation("#faq-header","fadeIn", 100, 80);
   scrollAnimation("#faq0","fadeInLeft", 200, 80);
   scrollAnimation("#faq1","fadeInRight", 200, 80);
-  scrollAnimation("#faq2","fadeInLeft", 200, 80);
-  scrollAnimation("#faq3","faUp", 200, 80);
-  scrollAnimation("#faq4","fadeInRight", 200, 80);
+  scrollAnimation("#faq2","fadeInLeft", 400, 80);
+  scrollAnimation("#faq3","fadeInUp", 400, 80);
+  scrollAnimation("#faq4","fadeInRight", 400, 80);
 });
--- public/js/animations/reserve-animations.js
+++ public/js/animations/reserve-animations.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   scrollAnimation("#reserve-header-text", "fadeIn", 200, 80);
-  scrollAnimation("#small", "fadeInUp", 300, 80);
-  scrollAnimation("#medium", "fadeInUp", 400, 80);
-  scrollAnimation("#large", "fadeInUp", 500, 80);
-  scrollAnimation("#x-large", "fadeInUp", 600, 80);
+  scrollAnimation("#small", "fadeInUp", 300, 99);
+  scrollAnimation("#medium", "fadeInUp", 400, 99);
+  scrollAnimation("#large", "fadeInUp", 500, 99);
+  scrollAnimation("#x-large", "fadeInUp", 600, 99);
 });
